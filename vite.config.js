70d355729fcd85d401f6c57b9adc327d190e2c3c
kazi mohammad Foysal <95936171+kmfoysal06@users.noreply.github.com>
2026-09-17 03:47:43 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/PH-Assignment-2/"
+  base: "/ph-kmf-movies/"
 })
